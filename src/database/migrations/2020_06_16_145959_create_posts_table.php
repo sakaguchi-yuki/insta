258af5b0ff65dbd53d3e021b051b111a125506b7
sakaguchi-yuki <sakaguchiyuki@team-lab.com>
2020-06-17 00:15:07 +0900

    ユーザー、投稿テーブル

--- src/database/migrations/2020_06_16_145959_create_posts_table.php
+++ src/database/migrations/2020_06_16_145959_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique()->null()->comment('ユーザ名');
-            $table->string('profile_image')->nullable()->comment('プロフィール画像');
+            $table->increments('user_id');
+            $table->string('path');
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
