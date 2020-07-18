13d047398a30a22ac115575556a69f77f81603da
sakaguchi-yuki <sakaguchiyuki@team-lab.com>
2020-07-18 23:43:04 +0900

    アイコンとユーザー名

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Socialite;
 
 class ProfileController extends Controller
 {
-    public function index(){
-      return view('profile');
+    public function index(Request $request){
+        $token = $request->session()->get('github_token', null);
+        try {
+            $user = Socialite::driver('github')->userFromToken($token);
+        } catch (\Exception $e) {
+            return redirect('login/github');
+        }
+
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('GET', 'https://api.github.com/user/repos', [
+            'auth' => [$user->user['login'], $token]
+        ]);
+      $posts = \App\Models\Post::all();
+      $data = [
+          'posts' => $posts,
+	  'username' => $user->nickname
+      ];      
+      return view('profile',$data);
     }
 }
--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -1,1 +1,3 @@
 <h1>プロフィール画面</h1>
+<img src="https://avatars.githubusercontent.com/{{ $username }}">
+{{ $username }}
